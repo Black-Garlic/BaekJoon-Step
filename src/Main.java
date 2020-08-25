6db204b1aadb561dfe9fd4c4533be69e3fe12ccd
Black_Garlic <hn1226@naver.com>
2020-08-25 20:57:39 +0900

    2020-08-24 Step A - 입출력과 사칙연산

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,149 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		A01();
 	}
-
+	/*
+	// FrameWork
+	// A. 입출력과 사칙연산
+	// 1. Hello World 
+	// Import 
+	private static void A01() {
+		System.out.println("Hello World!");
+	}
+	*/
+	
+	// A. 입출력과 사칙연산
+	// 1. Hello World - 2557
+	// Import 
+	private static void A01() {
+		System.out.println("Hello World!");
+	}
+	
+	// A. 입출력과 사칙연산
+	// 2. We love Kriii - 10718
+	// Import 
+	private static void A02() {
+		System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+	}	
+	
+	// A. 입출력과 사칙연산
+	// 3. 고양이 - 10171
+	// Import 
+	private static void A03() {
+		System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+	}
+	
+	// A. 입출력과 사칙연산
+	// 4. 개 - 10172
+	// Import 
+	private static void A04() {
+		System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+	}
+	
+	// A. 입출력과 사칙연산
+	// 5. A+B - 1000
+	// Import Scanner
+	private static void A05() {
+		Scanner scanner = new Scanner(System.in);
+		int a, b;
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+		System.out.println(a+b);
+	}
+	
+	// A. 입출력과 사칙연산
+	// 6. A-B - 1001
+	// Import Scanner
+	private static void A06() {
+		Scanner scanner = new Scanner(System.in);
+		int a, b;
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+		System.out.println(a-b);
+	}
+	
+	// A. 입출력과 사칙연산
+	// 7. AxB - 10998
+	// Import Scanner
+	private static void A07() {
+		Scanner scanner = new Scanner(System.in);
+		int a, b;
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+		System.out.println(a*b);
+	}
+	
+	// A. 입출력과 사칙연산
+	// 8. A/B - 1008
+	// Import Scanner
+	private static void A08() {
+		Scanner scanner = new Scanner(System.in);
+		int a, b;
+        
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+        
+		System.out.println((double)a/b);
+	}
+	
+	// A. 입출력과 사칙연산
+	// 9. 사칙연산
+	// Import Scanner
+	private static void A09() {
+		Scanner scanner = new Scanner(System.in);
+		int a, b;
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+	
+	// A. 입출력과 사칙연산
+	// 10. 나머지 - 10430
+	// Import Scanner
+	private static void A10() {
+		Scanner scanner = new Scanner(System.in);
+		int A, B, C;
+		A = scanner.nextInt();
+		B = scanner.nextInt();
+		C = scanner.nextInt();
+		System.out.println((A+B)%C);
+		System.out.println(((A%C) + (B%C))%C);
+		System.out.println((A*B)%C);
+		System.out.println(((A%C)*(B%C))%C);
+	}
+	
+	// A. 입출력과 사칙연산
+	// 11. 곱셈 - 2588
+	// Import Scanner
+	private static void A11() {
+		Scanner scanner = new Scanner(System.in);
+		int a, b, hundred, ten, one;
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+		
+		hundred = b / 100;
+		ten = (b - hundred * 100) / 10;
+		one = b % 10;
+		
+		System.out.println(a * one);
+		System.out.println(a * ten);
+		System.out.println(a * hundred);
+		System.out.println(a * b);
+	}
+	
 }
