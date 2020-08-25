ecc914fc4e8c03dcea5939e9e3248bd5671946f8
Black_Garlic <hn1226@naver.com>
2020-08-25 21:04:39 +0900

    2020-08-24 Step B - if문

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 import java.util.Scanner;
 
 public class Main {
-
-	public static void main(String[] args) {
-		A01();
-	}
 	/*
-	// FrameWork
+	// Frame
 	// A. 입출력과 사칙연산
 	// 1. Hello World 
 	// Import 
@@ -14,6 +10,111 @@ public class Main {
 		System.out.println("Hello World!");
 	}
 	*/
+	
+	public static void main(String[] args) {
+
+	}
+	
+	// B. if문
+	// 1. 두 수 비교하기 - 1330 
+	// Import Scanner
+	private static void B01() {
+		Scanner scanner = new Scanner(System.in);
+		int a, b;
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+		
+		if (a > b) {
+			System.out.println(">");
+		} else if (a == b) {
+			System.out.println("==");
+		} else {
+			System.out.println("<");
+		}
+	}
+
+	// B. if문
+	// 2. 시험 성적 - 9498 
+	// Import Scanner
+	private static void B02() {
+		Scanner scanner = new Scanner(System.in);
+		int a;
+		a = scanner.nextInt();
+		
+		if (a >= 90) {
+			System.out.println("A");
+		} else if (a >= 80) {
+			System.out.println("B");
+		} else if (a >= 70) {
+			System.out.println("C");
+		} else if (a >= 60) {
+			System.out.println("D");
+		} else {
+			System.out.println("F");
+		}
+	}
+	
+	// B. if문
+	// 3. 윤년 - 2753 
+	// Import Scanner
+	private static void B03() {
+		Scanner scanner = new Scanner(System.in);
+		int a;
+		a = scanner.nextInt();
+		
+		if (a % 4 == 0) {
+			if (a % 100 == 0 && a % 400 != 0) {
+				System.out.println("0");	
+			} else {
+				System.out.println("1");	
+			}
+		} else {
+			System.out.println("0");
+		}
+	}
+	
+	// B. if문
+	// 4. 사분면 고르기 - 14681 
+	// Import Scanner
+	private static void B04() {
+		Scanner scanner = new Scanner(System.in);
+		int x, y;
+		x = scanner.nextInt();
+		y = scanner.nextInt();
+		
+		if (x > 0) {
+			if (y > 0) {
+				System.out.println("1");	
+			} else {
+				System.out.println("4");	
+			}
+		} else {
+			if (y > 0) {
+				System.out.println("2");	
+			} else {
+				System.out.println("3");	
+			}
+		}
+	}
+	
+	// B. if문
+	// 5. 알람 시계 - 2884 
+	// Import Scanner
+	private static void B05() {
+		Scanner scanner = new Scanner(System.in);
+		int x, y, time, hour, minute;
+		x = scanner.nextInt();
+		y = scanner.nextInt();
+		
+		time = x * 60 + y - 45;
+        if (time < 0) {
+			time = 1440 + time;
+		}
+		hour = time / 60;
+		minute = time % 60;
+		
+		System.out.print(hour + " " + minute);
+	}
 	
 	// A. 입출력과 사칙연산
 	// 1. Hello World - 2557
